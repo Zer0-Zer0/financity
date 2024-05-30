793d7f26f1eaa35928dae7a9da403d18bd8b1d67
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-05-29 22:20:33 -0300

    Added player jumping controller to the script separation procedure

---
+++ Assets/Prefabs/Player/Scripts/PlayerJumpingController.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpingController : MonoBehaviour
+{
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private Animator jumpAnimation;
+
+    [SerializeField] private LayerMask layerGround;
+    [SerializeField] private Transform groundChecker;
+    [SerializeField] private float radiusChecker;
+
+    [SerializeField] private float maxHeight;
+    [SerializeField] private float timeToMaxHeight;
+    private Vector3 _yJumpForce;
+    private float _jumpSpeed;
+    private float _gravity;
+    private float move;
+    [SerializeField] private float forwardJumpDistance;
+
+    private void Start()
+    {
+        SetGravity();
+    }
+
+    private void Update()
+    {
+        JumpForce();
+        GravityForce();
+        move = Input.GetAxis("Vertical");
+    }
+
+    private void SetGravity()
+    {
+        _gravity = (2 * maxHeight) / Mathf.Pow(timeToMaxHeight, 2);
+        _jumpSpeed = _gravity * timeToMaxHeight;
+    }
+
+    private void GravityForce()
+    {
+        _yJumpForce += _gravity * Time.deltaTime * Vector3.down;
+        characterController.Move(_yJumpForce);
+
+        if (IsGrounded()) _yJumpForce = Vector3.zero;
+    }
+
+    private void JumpForce()
+    {
+        if (IsGrounded())
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yJumpForce = _jumpSpeed * Vector3.up;
+
+                Vector3 horizontalJumpForce = Vector3.zero;
+                if (move != 0)
+                {
+                    horizontalJumpForce = transform.forward * forwardJumpDistance;
+                }
+
+                _yJumpForce += horizontalJumpForce * _jumpSpeed;
+
+                characterController.Move(_yJumpForce);
+
+                if (move != 0)
+                {
+                    jumpAnimation.SetBool("puloCorrida", true);
+                }
+                else
+                {
+                    jumpAnimation.SetBool("jump", true);
+                }
+            }
+            else
+            {
+                jumpAnimation.SetBool("jump", false);
+                jumpAnimation.SetBool("puloCorrida", false);
+            }
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        bool isGrounded = Physics.CheckSphere(groundChecker.position, radiusChecker, layerGround);
+        return isGrounded;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(groundChecker.position, radiusChecker);
+    }
+}
