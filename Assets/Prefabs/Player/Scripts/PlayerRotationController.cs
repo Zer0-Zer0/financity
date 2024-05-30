490c905362133e25a2c0d04216d906b0dd851021
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-05-30 08:13:17 -0300

    Made the rotation controller work

--- Assets/Prefabs/Player/Scripts/PlayerRotationController.cs
+++ Assets/Prefabs/Player/Scripts/PlayerRotationController.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class PlayerRotationController : MonoBehaviour
 {
-    [SerializeField] private Transform wall;
-    [SerializeField] private float rotationSpeed = 100;
+        [SerializeField] private float rotationSpeed = 100;
 
-    void AdjustRotation()
-    {
-        if (Vector3.Distance(transform.position, wall.position) <= 3.1f)
-        {
-            transform.rotation = Quaternion.Lerp(transform.rotation, wall.rotation, 1f);
-        }
-    }
+    public GameObject playerSpine;
+
+    public float fov = 60f;
+    public bool invertCamera = false;
+    public bool cameraCanMove = true;
+    public float mouseSensitivity = 2f;
+    public float maxLookAngle = 50f;
+
+    // Internal Variables
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
 
     void FixedUpdate()
     {
+        if(cameraCanMove)
+        {
+            yaw = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
+
+            if (!invertCamera)
+            {
+                pitch -= mouseSensitivity * Input.GetAxis("Mouse Y");
+            }
+            else
+            {
+                // Inverted Y
+                pitch += mouseSensitivity * Input.GetAxis("Mouse Y");
+            }
+
+            // Clamp pitch between lookAngle
+            pitch = Mathf.Clamp(pitch, -maxLookAngle, maxLookAngle);
+
+            playerSpine.transform.Rotate(0, yaw, 0);
+            transform.localEulerAngles = new Vector3(pitch, 0, 0);
+        }
+
         // Implement your rotation logic here
     }
 }
