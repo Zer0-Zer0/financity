2db049f740de39db8287a858da485f6f507a3482
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-06-01 19:34:29 -0300

    Added loan type enum and MakeALoan function

--- Assets/Scripts/Economy/LoanManager.cs
+++ Assets/Scripts/Economy/LoanManager.cs
@@ -6,26 +6,44 @@ using UnityEngine.Events;
 public class LoanManager : MonoBehaviour
 {
     /// <summary>
+    /// Represents the type of loan, which can be Simple Interest or Compound Interest.
+    /// </summary>
+    public enum LoanType
+    {
+        /// <summary>
+        /// Represents a loan with simple interest calculation.
+        /// </summary>
+        SimpleInterest,
+
+        /// <summary>
+        /// Represents a loan with compound interest calculation.
+        /// </summary>
+        CompoundInterest
+    }
+
+    /// <summary>
     /// Represents data for a loan, including total value, principal value, interest rate, installment value, and time value.
     /// </summary>
     /// <param name="TotalValue">The total value of the loan.</param>
     /// <param name="PrincipalValue">The principal value of the loan.</param>
     /// <param name="RateValue">The interest rate of the loan, in percent.</param>
     /// <param name="TimeValue">The time duration of the loan.</param>
+    /// <param name="loanType">The type of loan.</param>
     [System.Serializeable]
     public struct LoanData{
         internal readonly float TotalValue;
         internal readonly float PrincipalValue;
         internal readonly float RateValue;
         internal readonly int TimeValue;
+        internal readonly LoanType loanType;
+
+        /// <summary>
+        /// Calculates the installment amount for the loan.
+        /// </summary>
+        /// <returns>The installment amount for the loan.</returns>
         internal readonly float GetInstallment(){
             return TotalValue/TimeValue;
         }
-    }
-
-    public static enum LoanType{
-        SimpleInterest,
-        CompountInterest
     }
 
     /// <summary>
@@ -55,13 +73,22 @@ public class LoanManager : MonoBehaviour
         return new LoanData(_totalValue, principal, rate, time);
     }
 
+    /// <summary>
+    /// Makes a loan for the player.
+    /// </summary>
+    /// <param name="Wallet">The wallet manager of the player.</param>
+    /// <param name="loanData">The data of the loan to be made.</param>
+    /// <param name="loanType">The type of loan to be made.</param>
+    /// <remarks>
+    /// If the new debt exceeds the current maximum debt, an error is logged.
+    /// </remarks>
     public static void MakeALoan(WalletManager Wallet, LoanData loanData, LoanType loanType){
-        switch (loanType){
-            case LoanType.SimpleInterest:
-            
-            break;
-            case LoanType.CompountInterest:
-            break;
+        float newDebt = Wallet.CurrentDebt + loanData.TotalValue;
+        if(newDebt > Wallet.CurrentMaxDebt){
+            Debug.LogError("ERRO! Tentativa de fazer empréstimo quando débito máximo é menor que o débito atual, adicione mais condicionais");
+        }else{
+            Wallet.CurrentDebt = newDebt;
+            Wallet.CurrentDigitalMoney += LoanData.PrincipalValue; 
         }
     }
 }
