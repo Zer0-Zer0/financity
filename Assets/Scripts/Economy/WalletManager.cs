bd62522703878c76af9f5cbc793962f1c345dfb5
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-06-01 17:03:15 -0300

    Added debt to the wallet

--- Assets/Scripts/Economy/WalletManager.cs
+++ Assets/Scripts/Economy/WalletManager.cs
@@ -7,6 +7,8 @@ class WalletManager : MonoBehaviour
 {
     [SerializeField] float _initialDigitalMoney = 800f;
     [SerializeField] float _initialPhysicalMoney = 0f;
+    [SerializeField] float _initialDebt = 800f;
+    [SerializeField] float _initialMaxDebt = 800f;
 
     float _currentDigitalMoney;
 
@@ -33,7 +35,7 @@ class WalletManager : MonoBehaviour
         set{
             OnPhysicalMoneyUpdate.Invoke();
             if(value < 0f){
-                Debug.LogError("ERRO! Tentativa de comprar algo com dinheiro físico e valor ficou negativado, alguém esqueceu um if em algum lugar <-<");
+                Debug.LogError("ERRO! Tentativa de comprar algo com dinheiro físico e valor ficou negativado, alguém esqueceu de um if em algum lugar <-<");
             }
             if(value > _currentPhysicalMoney){
                 OnPhysicalMoneyIncrease.Invoke();
@@ -46,6 +48,48 @@ class WalletManager : MonoBehaviour
         }
     }
 
+    float _currentDebt;
+
+    public float CurrentDebt{
+        get; 
+        set{
+            OnDebtUpdate.Invoke();
+            if(value < 0f){
+                Debug.LogError("ERRO! Tentativa de ter um débito negativo, alguém esqueceu de um if em algum lugar <-<");
+            }else if (value > CurrentMaxDebt){
+                Debug.LogError("ERRO! Tentativa de ter um débito maior que o permitido, alguém esqueceu de um if em algum lugar <-<");
+            }
+            if(value > _currentDebt){
+                OnDebtIncrease.Invoke();
+            }
+            else if (value < _currentDebt)
+            {
+                OnDebtReduce.Invoke();
+            }
+            _currentDebt = value;
+        }
+    }
+
+    float _currentMaxDebt;
+
+    public float CurrentMaxDebt{
+        get; 
+        set{
+            OnMaxDebtUpdate.Invoke();
+            if(value < 0f){
+                Debug.LogError("ERRO! Tentativa de ter um débito máximo negativo, alguém esqueceu de um if em algum lugar <-<");
+            }
+            if(value > _currentMaxDebt){
+                OnMaxDebtIncrease.Invoke();
+            }
+            else if (value < _currentMaxDebt)
+            {
+                OnMaxDebtReduce.Invoke();
+            }
+            _currentMaxDebt = value;
+        }
+    }
+
     public UnityEvent OnDigitalMoneyIncrease;
     public UnityEvent OnDigitalMoneyUpdate;
     public UnityEvent OnDigitalMoneyReduce;
@@ -54,8 +98,18 @@ class WalletManager : MonoBehaviour
     public UnityEvent OnPhysicalMoneyUpdate;
     public UnityEvent OnPhysicalMoneyReduce;
 
+    public UnityEvent OnDebtIncrease;
+    public UnityEvent OnDebtUpdate;
+    public UnityEvent OnDebtReduce;
+
+    public UnityEvent OnMaxDebtIncrease;
+    public UnityEvent OnMaxDebtUpdate;
+    public UnityEvent OnMaxDebtReduce;
+
     void Start(){
         CurrentDigitalMoney = _initialDigitalMoney;
         CurrentPhysicalMoney = _initialPhysicalMoney;
+        CurrentDebt = _initialDebt;
+        CurrentMaxDebt = _initialMaxDebt;
     }
 }
