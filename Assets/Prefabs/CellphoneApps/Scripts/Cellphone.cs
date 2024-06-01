4775a0b5c4a93c5f469fb8683c15ac56c31147a0
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-06-01 10:57:36 -0300

    Began making the app system

---
+++ Assets/Prefabs/CellphoneApps/Scripts/Cellphone.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cellphone : MonoBehaviour
+{
+    public Transform AppsParent;
+
+    [SerializeField] private Button MenuButton;
+
+    public List<GenericApp> AppsList;
+
+    void Awake()
+    {
+        //Transfers all children (they NEED to have the GenericApp script)from the given object to a list
+        
+        foreach (Transform child in AppsParent)
+        {
+            GenericApp app = child.GetComponent<GenericApp>();
+            AppsList.Add(app);
+        }
+    }
+
+    //Hides all the app panels
+    public void HideAllApps(){
+        foreach (GenericApp app in AppsList)
+        {
+            app.AppPanel.SetActive(false);
+        }
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+}
