4bdf410a7e401aa3452492f2e92a869b4b927af5
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-06-07 07:37:35 -0300

    Converted materials to URP

---
+++ Assets/Prefabs/CellphoneApps/Scripts/CellphoneTweenControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellphoneTweenControl : MonoBehaviour
+{
+
+    float upOrDown = 1f;
+    // Update is called once per frame
+    void Update()
+    {   
+        if (Input.GetKeyDown(KeyCode.E)){
+            MoveObject();
+        }
+    }
+
+    void MoveObject()
+    {
+        LeanTween.moveY(gameObject.GetComponent<RectTransform>(), 396 * upOrDown, 0.3f);
+        upOrDown *=-1f;
+    }
+}
