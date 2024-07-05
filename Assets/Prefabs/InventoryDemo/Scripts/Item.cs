5283714d9a3a066306e496f6258a78e65a34377b
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-07-05 20:47:18 -0300

    Added a description and sprite options

--- Assets/Prefabs/InventoryDemo/Scripts/Item.cs
+++ Assets/Prefabs/InventoryDemo/Scripts/Item.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 public class InventoryItem : ScriptableObject
 {
     public string Name;
     public int MaxAmount;
+    public string Description;
+    public Sprite Icon;
 
     public override string ToString()
     {
