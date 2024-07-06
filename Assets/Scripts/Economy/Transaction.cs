940a2273917cb45f6e69cd46dc6c143e222c630f
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-07-06 14:21:12 -0300

    Comments

--- Assets/Scripts/Economy/Transaction.cs
+++ Assets/Scripts/Economy/Transaction.cs
@@ -4,9 +4,16 @@ using System;
 using UnityEngine;
 using UnityEngine.Events;
 
+
+/// <summary>
+/// Represents a transaction between two wallets.
+/// </summary>
 [System.Serializable]
 public struct Transaction
 {
+    /// <summary>
+    /// The name of the transaction.
+    /// </summary>
     [SerializeField] private string _name;
     public string Name
     {
@@ -19,6 +26,10 @@ public struct Transaction
             _name = value;
         }
     }
+
+    /// <summary>
+    /// The value of the transaction.
+    /// </summary>
     [SerializeField] private float _value;
     public float Value
     {
@@ -31,6 +42,10 @@ public struct Transaction
             _value = value;
         }
     }
+
+    /// <summary>
+    /// The wallet data of the sender.
+    /// </summary>
     [SerializeField] private WalletData _sender;
     public WalletData Sender
     {
@@ -43,6 +58,10 @@ public struct Transaction
             _sender = value;
         }
     }
+
+    /// <summary>
+    /// The wallet data of the receiver.
+    /// </summary>
     [SerializeField] private WalletData _receiver;
     public WalletData Receiver
     {
@@ -56,6 +75,9 @@ public struct Transaction
         }
     }
 
+    /// <summary>
+    /// The type of the transaction.
+    /// </summary>
     [SerializeField] private Type _transactionType;
     public Type TransactionType
     {
@@ -69,16 +91,38 @@ public struct Transaction
         }
     }
 
+    /// <summary>
+    /// Enum representing the type of transaction.
+    /// </summary>
     public enum Type
     {
         Physical,
         Digital
     }
 
+    /// <summary>
+    /// Event triggered when a transaction is pending.
+    /// </summary>
     public UnityEvent<Transaction> OnTransactionPending;
+
+    /// <summary>
+    /// Event triggered when a transfer is accepted.
+    /// </summary>
     public UnityEvent<Transaction> OnTransferAccepted;
+
+    /// <summary>
+    /// Event triggered when a transfer is refused.
+    /// </summary>
     public UnityEvent<Transaction> OnTransferRefused;
 
+    /// <summary>
+    /// Constructor for creating a new transaction.
+    /// </summary>
+    /// <param name="name">The name of the transaction.</param>
+    /// <param name="value">The value of the transaction.</param>
+    /// <param name="sender">The wallet data of the sender.</param>
+    /// <param name="receiver">The wallet data of the receiver.</param>
+    /// <param name="transactionType">The type of the transaction (Physical or Digital).</param>
     public Transaction(string name, float value, WalletData sender, WalletData receiver, Type transactionType)
     {
         this._name = name;
