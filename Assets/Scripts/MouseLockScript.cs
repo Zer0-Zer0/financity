8d2c84508c9d08aeb9c413d913e9339c563daf75
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-07-22 21:23:09 -0300

    Mouse related scripts

---
+++ Assets/Scripts/MouseLockScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLockScript : MonoBehaviour
+{
+    private bool _isCursorLocked = true;
+    void Start()
+    {
+        SetCursorState(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            _isCursorLocked = !_isCursorLocked;
+            SetCursorState(_isCursorLocked);
+        }
+    }
+
+    void SetCursorState(bool isCursorLocked)
+    {
+        if (isCursorLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
--- Assets/Scripts/MouseVisibilityToggle.cs
+++ Assets/Scripts/MouseVisibilityToggle.cs
@@ -24,7 +24,6 @@ public class MouseVisibilityToggle : MonoBehaviour
             if (!cursorVisible)
             {
                 Cursor.lockState = CursorLockMode.Locked;
-                Cursor.lockState = CursorLockMode.None;
             }
             else
             {
