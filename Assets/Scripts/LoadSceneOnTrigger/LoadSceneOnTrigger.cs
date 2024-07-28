d14e268ccef23ec2a2df6649e2bc2e394f1b16b1
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-07-28 14:19:08 -0300

    Made a scene loader system

---
+++ Assets/Scripts/LoadSceneOnTrigger/LoadSceneOnTrigger.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnTrigger : MonoBehaviour
+{
+    [SerializeField] private string _sceneName;
+    private bool _isPlayerInRange = false;
+
+    public UnityEvent PlayerIsInRange;
+    public UnityEvent PlayerOutOfRange;
+
+    void Update()
+    {
+        if (_isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(_sceneName);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _isPlayerInRange = true;
+            PlayerIsInRange?.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _isPlayerInRange = false;
+            PlayerOutOfRange?.Invoke();
+        }
+    }
+}
