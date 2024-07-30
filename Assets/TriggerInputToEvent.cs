b79abb1d82726158bf8534b65eb65e2217430c7e
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-07-29 22:21:39 -0300

    MADE THE FIRST MISSION WORK

---
+++ Assets/TriggerInputToEvent.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerInputToEvent : MonoBehaviour
+{
+    public KeyCode ButtonToPress;
+    public UnityEvent EventToInvoke;
+    public UnityEvent PlayerInTrigger;
+    public UnityEvent PlayerOutOfTrigger;
+    private bool _isPlayerInRange;
+
+    void Update()
+    {
+        if (Input.GetKey(ButtonToPress) && _isPlayerInRange)
+        {
+            EventToInvoke?.Invoke();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerInTrigger?.Invoke();
+            _isPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerOutOfTrigger?.Invoke();
+            _isPlayerInRange = false;
+        }
+    }
+}
