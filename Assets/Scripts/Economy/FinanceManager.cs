27941574ce938af6daeefeeda127f22fd345d458
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-07-31 16:46:04 -0300

    Moving stuff around

---
+++ Assets/Scripts/Economy/FinanceManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FinanceManager : MonoBehaviour
+{
+    private float _currentBalance
+    {
+        get { return DataManager.playerData.GetCurrentBalance(); }
+        set { DataManager.playerData.SetCurrentBalance(value); }
+    }
+
+    // Adiciona uma compra à lista de transações
+    public static void AddPurchase(float amount)
+    {
+        _currentBalance -= amount;
+    }
+
+    // Adiciona um crédito à lista de transações
+    public static void AddCredit(float amount)
+    {
+        _currentBalance += amount;
+    }
+}
