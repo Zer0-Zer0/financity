250b69c9cec6e420e95d24d4042dfec9cc362b45
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-08-01 20:39:45 -0300

    Began on some event manager

---
+++ Assets/Scripts/EventSystem/GameEvent.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEvent", menuName = "GameEvent", order = 0)]
+public class GameEvent : ScriptableObject {
+    //Fonte de inspiração: https://youtu.be/7_dyDmF0Ktw
+
+    
+}
