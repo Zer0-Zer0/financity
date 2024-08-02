5b77222015b5557122bf88a345dcb8c72860e041
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-08-02 14:51:36 -0300

    Baked new lightmap

---
+++ Assets/Scenes/SpaceSafezone/Elevator/SafezoneElevator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SafezoneElevator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private UnityEvent ElevatorChegou;
+
+    [SerializeField]
+    private GameObject Player;
+
+    public void EmitirElevadorChegou()
+    {
+        ElevatorChegou?.Invoke();
+
+        if (Player != null)
+        {
+            Player.transform.SetParent(null);
+        }
+    }
+}
