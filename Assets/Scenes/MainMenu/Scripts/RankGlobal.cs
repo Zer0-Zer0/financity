9e767e8d5e87ea9487700a8b65b51f9aa13d67a6
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-08-04 15:56:51 -0300

    Revert "READD"
    This reverts commit 79607463156acf56b671e44ebe75f6b11e4d95bc.

---
+++ Assets/Scenes/MainMenu/Scripts/RankGlobal.cs
@@ -0,0 +1,57 @@
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using TMPro;
+using Microsoft.Unity.VisualStudio.Editor;
+
+public class RankGlobal : MonoBehaviour
+{
+    private string username = "V1ctorroff";
+    private string token = "fc2221eabfb8ecccabbaa7fa0c36fbb06da43a13";
+    public string filePath = "/home/V1ctorroff/Ranked.txt";
+    public TMP_Text displayText;
+    public float delay = 5f;
+
+    public GameObject rank;
+
+    public bool active = false;
+
+
+    public void FixedUpdate()
+    {
+        if (rank.activeSelf && !active)
+        {
+            active = !active;
+            displayText.gameObject.SetActive(true);
+            displayText.text = "                      CARREGANDO...";
+            StartCoroutine(GetFileAfterDelay(delay));
+        } else if (!rank.activeSelf && active) {
+            active = !active;
+            displayText.gameObject.SetActive(false);
+        }
+
+    }
+
+    IEnumerator GetFileAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        string url = "https://www.pythonanywhere.com/api/v0/user/" + username + "/files/path" + filePath;
+        using (UnityWebRequest www = UnityWebRequest.Get(url))
+        {
+            www.SetRequestHeader("Authorization", "Token " + token);
+
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.Success)
+            {
+                displayText.text = www.downloadHandler.text;
+            }
+            else
+            {
+                displayText.text = "Erro ao buscar " + www.error;
+            }
+        }
+    }
+}
