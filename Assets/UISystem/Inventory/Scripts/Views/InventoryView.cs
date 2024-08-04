a5ef2a532f807877d7fb6d2a482199dacb9cfe9a
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-08-04 15:01:37 -0300

    Made so the inventory dynamically adds slots to itself

--- Assets/UISystem/Inventory/Scripts/Views/InventoryView.cs
+++ Assets/UISystem/Inventory/Scripts/Views/InventoryView.cs
@@ -1,36 +1,88 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 namespace UISystem
 {
     public class InventoryView : CustomUIComponent
     {
-        [SerializeField] private Style _style;
-        [SerializeField] private Image _backgroundImage;
-        [SerializeField] private GameObject _InventorySlotsParent;
+        [SerializeField]
+        private Style _style;
+
+        [SerializeField]
+        private Image _backgroundImage;
+
+        [SerializeField]
+        private GameObject _InventorySlotsParent;
         private List<InventorySlotView> _InventorySlotViews;
 
-        private float _slotCount{ get{ return _InventorySlotViews.Count;} }
+        [SerializeField]
+        private InventorySlotView _InventorySlotViewPrefab; // Prefab for the inventory slot view
+
+        private int _slotCount
+        {
+            get { return _InventorySlotViews.Count; }
+        }
 
         protected override void Setup()
         {
-            _InventorySlotViews = new List<InventorySlotView>(_InventorySlotsParent.GetComponentsInChildren<InventorySlotView>());
+            _InventorySlotViews = new List<InventorySlotView>(
+                _InventorySlotsParent.GetComponentsInChildren<InventorySlotView>()
+            );
         }
 
         protected override void Configure()
         {
             ThemeSO theme = GetMainTheme();
-            if (theme == null) return;
+            if (theme == null)
+                return;
 
             _backgroundImage.color = theme.GetBackgroundColor(_style);
         }
 
-        public void UpdateSlotsGraphics(List<InventorySlot> slots){
-            for(int index = 0; index < _slotCount; index++){
+        public void UpdateSlotsGraphics(List<InventorySlot> slots)
+        {
+            AdjustSlotCount(slots.Count);
+            UpdateSlotGraphics(slots);
+        }
+
+        private void AdjustSlotCount(int targetCount)
+        {
+            if (targetCount > _slotCount)
+                AddSlots(targetCount - _slotCount);
+            else if (targetCount < _slotCount)
+                RemoveSlots(_slotCount - targetCount);
+        }
+
+        private void AddSlots(int slotsToAdd)
+        {
+            for (int i = 0; i < slotsToAdd; i++)
+            {
+                InventorySlotView newSlotView = Instantiate(
+                    _InventorySlotViewPrefab,
+                    _InventorySlotsParent.transform
+                );
+                _InventorySlotViews.Add(newSlotView);
+            }
+        }
+
+        private void RemoveSlots(int slotsToRemove)
+        {
+            for (int i = 0; i < slotsToRemove; i++)
+            {
+                InventorySlotView slotToRemove = _InventorySlotViews[_InventorySlotViews.Count - 1];
+                _InventorySlotViews.RemoveAt(_InventorySlotViews.Count - 1);
+                Destroy(slotToRemove.gameObject);
+            }
+        }
+
+        private void UpdateSlotGraphics(List<InventorySlot> slots)
+        {
+            for (int index = 0; index < slots.Count; index++)
+            {
                 _InventorySlotViews[index].UpdateSlotGraphics(slots[index]);
             }
         }
