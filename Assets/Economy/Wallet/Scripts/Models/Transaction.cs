e49a8d0ef0c555d05883fb237a6c647ad977cde8
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-08-10 14:25:27 -0300

    Made transaction imutable

--- Assets/Economy/Wallet/Scripts/Models/Transaction.cs
+++ Assets/Economy/Wallet/Scripts/Models/Transaction.cs
@@ -15,82 +15,32 @@ namespace Economy
         /// <summary>
         /// The name of the transaction.
         /// </summary>
-        [SerializeField] private string _name;
-        public string Name
-        {
-            get
-            {
-                return _name;
-            }
-            private set
-            {
-                _name = value;
-            }
-        }
+        [SerializeField] private readonly string _name;
+        public string Name => _name;
 
         /// <summary>
         /// The value of the transaction.
         /// </summary>
-        [SerializeField] private float _value;
-        public float Value
-        {
-            get
-            {
-                return _value;
-            }
-            private set
-            {
-                _value = value;
-            }
-        }
+        [SerializeField] private readonly float _value;
+        public float Value => _value;
 
         /// <summary>
         /// The wallet data of the sender.
         /// </summary>
-        [SerializeField] private WalletData _sender;
-        public WalletData Sender
-        {
-            get
-            {
-                return _sender;
-            }
-            private set
-            {
-                _sender = value;
-            }
-        }
+        [SerializeField] private readonly WalletData _sender;
+        public WalletData Sender => _sender;
 
         /// <summary>
         /// The wallet data of the receiver.
         /// </summary>
-        [SerializeField] private WalletData _receiver;
-        public WalletData Receiver
-        {
-            get
-            {
-                return _receiver;
-            }
-            private set
-            {
-                _receiver = value;
-            }
-        }
+        [SerializeField] private readonly WalletData _receiver;
+        public WalletData Receiver => _receiver;
 
         /// <summary>
         /// The type of the transaction.
         /// </summary>
-        [SerializeField] private TransactionType _type;
-        public TransactionType Type
-        {
-            get
-            {
-                return _type;
-            }
-            private set
-            {
-                _type = value;
-            }
-        }
+        [SerializeField] private readonly TransactionType _type;
+        public TransactionType Type => _type;
 
         /// <summary>
         /// Enum representing the type of transaction.
@@ -104,17 +54,17 @@ namespace Economy
         /// <summary>
         /// Event triggered when a transaction is pending.
         /// </summary>
-        public UnityEvent<Transaction> OnTransactionPending;
+        public UnityEvent<Transaction> OnTransactionPending { get; }
 
         /// <summary>
         /// Event triggered when a transfer is accepted.
         /// </summary>
-        public UnityEvent<Transaction> OnTransactionAccepted;
+        public UnityEvent<Transaction> OnTransactionAccepted { get; }
 
         /// <summary>
         /// Event triggered when a transfer is refused.
         /// </summary>
-        public UnityEvent<Transaction> OnTransactionRefused;
+        public UnityEvent<Transaction> OnTransactionRefused { get; }
 
         /// <summary>
         /// Constructor for creating a new transaction.
@@ -126,11 +76,11 @@ namespace Economy
         /// <param name="transactionType">The type of the transaction (Physical or Digital).</param>
         public Transaction(float value, WalletData sender, WalletData receiver, TransactionType type, string name = "")
         {
-            this._name = name;
-            this._value = value;
-            this._sender = sender;
-            this._receiver = receiver;
-            this._type = type;
+            _name = name;
+            _value = value;
+            _sender = sender;
+            _receiver = receiver;
+            _type = type;
 
             OnTransactionPending = new UnityEvent<Transaction>();
             OnTransactionAccepted = new UnityEvent<Transaction>();
