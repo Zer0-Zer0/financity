5bd6bca9bf702b838064b3590e9ee41d789d4e3c
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-08-11 15:55:35 -0300

    Trying to fix inventory related stuff

--- Assets/UISystem/Inventory/Scripts/ViewModels/InventoryViewModel.cs
+++ Assets/UISystem/Inventory/Scripts/ViewModels/InventoryViewModel.cs
@@ -1,16 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UISystem;
 using Inventory;
+using UISystem;
+using UnityEngine;
+
 public class InventoryViewModel : MonoBehaviour
 {
-    [SerializeField] private InventoryView _inventoryView;
+    [SerializeField]
+    InventoryView _inventoryView;
+
+    [SerializeField]
+    List<InventorySlot> _inventorySlots;
 
     public void OnInventoryChanged(Component sender, object data)
     {
         if (data is List<InventorySlot> slots)
-            _inventoryView.UpdateSlotsGraphics(slots);
+            _inventorySlots = slots;
+        UpdateSlotsGraphics();
+    }
+
+    private void OnEnable() => UpdateSlotsGraphics();
+
+    private void UpdateSlotsGraphics()
+    {
+        _inventoryView.UpdateSlotsGraphics(_inventorySlots);
     }
 }
