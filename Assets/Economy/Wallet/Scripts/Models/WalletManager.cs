d3f4f2d7822ace97895b5e75fe16013a3071e678
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-08-11 18:59:40 -0300

    Simplifed wallet manager

--- Assets/Economy/Wallet/Scripts/Models/WalletManager.cs
+++ Assets/Economy/Wallet/Scripts/Models/WalletManager.cs
@@ -21,65 +21,6 @@ namespace Economy
         /// </summary>
         public WalletData Wallet;
 
-        public Transaction MakeTransaction(float value, WalletData receiver, TransactionType type)
-        {
-            Transaction transactionToMake = new Transaction(value, type, Wallet, receiver);
-            TransactionValidation(transactionToMake);
-            transactionToMake.OnTransactionAccepted.AddListener(OnTransactionAcceptedEventHandler);
-            return transactionToMake;
-        }
-
-        public void ReceiveTransaction(Transaction transaction)
-        {
-            TransactionValidation(transaction);
-            transaction.OnTransactionAccepted?.Invoke(transaction);
-        }
-
-        private TransactionPosition VerifyTransactionPosition(Transaction transaction)
-        {
-            if (transaction.Sender == Wallet)
-                return TransactionPosition.Sender;
-            else if (transaction.Receiver == Wallet)
-                return TransactionPosition.Receiver;
-            else
-                throw new InvalidOperationException(
-                    "Transaction verification failed: Wallet is neither sender nor receiver."
-                );
-        }
-
-        private void TransactionValidation(Transaction transaction)
-        {
-            VerifySenderMoney(transaction);
-            VerifyTransactionPosition(transaction);
-        }
-
-        private static void VerifySenderMoney(Transaction transaction)
-        {
-            float senderBalance =
-                transaction.Type == TransactionType.Physical
-                    ? transaction.Sender.CurrentPhysicalMoney
-                    : transaction.Sender.CurrentDigitalMoney;
-
-            if (senderBalance < transaction.Value)
-            {
-                throw new InvalidOperationException(
-                    $"Transaction validation failed: Insufficient funds in sender's wallet for {transaction.Type} transaction."
-                );
-            }
-        }
-
-        private void OnTransactionAcceptedEventHandler(Transaction transaction)
-        {
-            UpdateWallets(transaction);
-            transaction.OnTransactionAccepted?.RemoveListener(OnTransactionAcceptedEventHandler);
-        }
-
-        private void UpdateWallets(Transaction transaction)
-        {
-            transaction.Receiver.Transactions.Add(transaction);
-            transaction.Sender.Transactions.Add(transaction);
-        }
-
         public void OnLoanRecieved(Component sender, object data)
         {
             if (data is LoanProcessor loanProcessor)
