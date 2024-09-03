2ed1ffc01d6f85994755013d275ffbc255068a1d
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-09-03 16:10:07 -0300

    feat: make end of the day prototype work

--- Assets/UISystem/EndOfTheDay/Scripts/View/EOTDView.cs
+++ Assets/UISystem/EndOfTheDay/Scripts/View/EOTDView.cs
@@ -1,21 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Economy;
+using System;
 
 namespace UISystem
 {
-    public class EOTDView : MonoBehaviour
+    public class EOTDView : CustomUIComponent
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField] Text ExpensesOfDay;
+        [SerializeField] Text ProfitsOfDay;
+        [SerializeField] Text IncomeOfDay;
 
+        protected override void Setup() { }
+
+        protected override void Configure() { }
+
+        private void SetExpenses(WalletData wallet)
+        {
+            foreach (var transaction in wallet.Transactions)
+            {
+                if (transaction.Receiver == wallet)
+                    continue;
+
+                string text = ExpensesOfDay.GetText();
+                text = $"{text}\n{transaction.Name} : -{transaction.Value}";
+                ExpensesOfDay.SetText(text);
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private void SetProfits(WalletData wallet)
         {
+            foreach (var transaction in wallet.Transactions)
+            {
+                if (transaction.Sender == wallet)
+                    continue;
 
+                string text = ProfitsOfDay.GetText();
+                text = $"{text}\n{transaction.Name} : {transaction.Value}";
+                ProfitsOfDay.SetText(text);
+            }
+        }
+
+        private void SetIncome(WalletData wallet)
+        {
+            string text = String.Format("{0:N2}BRL", wallet.CurrentMoney);
+            IncomeOfDay.SetText(text);
+        }
+
+        public void ClearText()
+        {
+            ExpensesOfDay.SetText("");
+            ProfitsOfDay.SetText("");
+            IncomeOfDay.SetText("");
+        }
+
+        public void SetData(WalletData wallet)
+        {
+            SetExpenses(wallet);
+            SetProfits(wallet);
+            SetIncome(wallet);
         }
     }
 }
---
+++ Assets/UISystem/EndOfTheDay/Scripts/ViewModel/EOTDViewModel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UISystem;
+
+public class EOTDViewModel : MonoBehaviour
+{
+    [SerializeField] EOTDView GetEOTDView;
+
+    public void OnEndOfTheDayEvent(Component sender, object data)
+    {
+        GetEOTDView.ClearText();
+        GetEOTDView.SetData(DataManager.playerData.GetCurrentWallet());
+    }
+}
