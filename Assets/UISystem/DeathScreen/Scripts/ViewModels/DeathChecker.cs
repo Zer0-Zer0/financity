a9ac0b06ff3f4a7e4661ea80b9a69a456ff50e94
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-09-05 17:29:27 -0300

    feat: add a check in the beginning of the death checker

--- Assets/UISystem/DeathScreen/Scripts/ViewModels/DeathChecker.cs
+++ Assets/UISystem/DeathScreen/Scripts/ViewModels/DeathChecker.cs
@@ -6,10 +6,24 @@ using UnityEngine.Events;
 public class DeathChecker : MonoBehaviour
 {
     [SerializeField] UnityEvent OnDeath;
+    float health;
+    bool canDie = false;
+    IEnumerator Start()
+    {
+        health = DataManager.playerData.GetCurrentHealth();
+        yield return 1f;
+        canDie = true;
+    }
+
     public void CheckAlive()
     {
-        float health = DataManager.playerData.GetCurrentHealth();
+        if (!canDie) return;
+
+        health = DataManager.playerData.GetCurrentHealth();
         if (health <= 0)
+        {
+            Debug.Log("Ded");
             OnDeath?.Invoke();
+        }
     }
 }
