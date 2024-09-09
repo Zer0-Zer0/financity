d1f401ac2471916d33ee1413c702ced6f19bf5de
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-09-09 20:23:55 -0300

    feat: make simple implementation of the "you owe x" screen

--- Assets/UISystem/EndOfTheDay/Scripts/View/YouOweView.cs
+++ Assets/UISystem/EndOfTheDay/Scripts/View/YouOweView.cs
@@ -1,18 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class YouOweView : MonoBehaviour
+namespace UISystem
 {
-    // Start is called before the first frame update
-    void Start()
+    public class YouOweView : CustomUIComponent
     {
-        
-    }
+        [Header("Objetos")]
+        [SerializeField] Text Titulo;
+        [SerializeField] Text Valor;
+        [SerializeField] Text Subtitulo;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("Valores")]
+        [SerializeField] string[] FrasesDeEfeito;
+
+        protected override void Setup() { }
+
+        protected override void Configure()
+        {
+            float _valor = DataManager.playerData.GetCurrentWallet().CurrentDebt;
+            float _parcelas = GetInstallments();
+
+            Titulo.SetText(SelectRandomPhrase());
+            Valor.SetText(FormatMoney(_valor));
+            Subtitulo.SetText(FormatSubtitle(_parcelas));
+        }
+
+        float GetInstallments()
+        {
+            float result;
+            if (DataManager.playerData.GetCurrentWallet().Loans.Count != 0)
+                result = DataManager.playerData.GetCurrentWallet().Loans[0].TotalRemainingInstallments;
+            else
+                result = -1;
+            return result;
+        }
+
+        string SelectRandomPhrase()
+        {
+            if (FrasesDeEfeito.Length == 0)
+            {
+                Debug.LogError("Sem frases de efeito para mostrar");
+                return "Sem frases de efeito para mostrar";
+            }
+            int randomIndex = UnityEngine.Random.Range(0, FrasesDeEfeito.Length);
+            return FrasesDeEfeito[randomIndex];
+        }
+
+        string FormatMoney(float value)
+        {
+            string result = String.Format("{0:N2}BRL", value);
+            return result;
+        }
+
+        string FormatSubtitle(float installments)
+        {
+            string result;
+            if (installments == -1)
+                result = $"Para pagar parcela nenhuma";
+            else if (installments == 1)
+                result = $"Para pagar sua última parcela!!!";
+            else
+                result = $"Para pagar suas {installments} parcelas!";
+            return result;
+        }
     }
 }
