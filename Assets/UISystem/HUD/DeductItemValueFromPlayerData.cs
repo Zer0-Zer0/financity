320700642572a6cf4cd708c4ab99f37a15416680
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-09-14 16:09:11 -0300

    feat: simplify a hud system
    remove the player data events reference in favor of a reference to player data directly

--- Assets/UISystem/HUD/DeductItemValueFromPlayerData.cs
+++ Assets/UISystem/HUD/DeductItemValueFromPlayerData.cs
@@ -4,19 +4,14 @@ using InventorySystem;
 using UnityEngine;
 using Economy;
 
-[RequireComponent(typeof(PlayerDataEvents))]
 public class DeductItemValueFromPlayerData : MonoBehaviour
 {
-    private PlayerDataEvents playerDataEvents;
-
-    private void OnEnable() => playerDataEvents = GetComponent<PlayerDataEvents>();
-
     public void DeductValueFromPlayerData(Component sender, object data)
     {
         if (data is InventorySlot slot)
         {
             Transaction transaction = new Transaction(slot.CurrentItem.GetCurrentValue(), TipoDeTransação.Remoção, slot.CurrentItem.Name);
-            playerDataEvents.AddTransaction(transaction);
+            DataManager.playerData.AddTransaction(transaction);
         }
     }
 }
