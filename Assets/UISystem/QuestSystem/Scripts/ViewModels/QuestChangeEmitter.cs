0b2e849c8e916e062f501f913630906f78503311
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-09-14 20:03:47 -0300

    chore: move quest change emitter to viewModel

---
+++ Assets/UISystem/QuestSystem/Scripts/ViewModels/QuestChangeEmitter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QuestSystem
+{
+    public class QuestChangeEmitter : MonoBehaviour
+    {
+        [SerializeField] GameEvent OnQuestChanged;
+        [SerializeField] string NewObjective;
+        void OnEnable() => VerifyUsage();
+
+        public void ChangeQuest() => OnQuestChanged.Raise(this, NewObjective);
+
+        private void VerifyUsage()
+        {
+            if (NewObjective == "")
+                Debug.LogWarning($"{gameObject.name}: QuestChangeEmitter used in a uninteded manner, its objective is null, use GameEventEmitter in these cases instead");
+        }
+    }
+}
