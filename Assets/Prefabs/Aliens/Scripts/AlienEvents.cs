332558b286174000cb6a6066ee981a425c7cfbaf
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-09-18 08:18:53 -0300

    fix: dumb aliens respawn when one of them spawns

--- Assets/Prefabs/Aliens/Scripts/AlienEvents.cs
+++ Assets/Prefabs/Aliens/Scripts/AlienEvents.cs
@@ -5,9 +5,14 @@ public class AlienEvents : MonoBehaviour
 {
     [SerializeField] List<AudioClip> Footsteps;
     [SerializeField] AudioSource audioSource;
+    [SerializeField] List<Component> scripts;
     public void PlayFootstepSound()
     {
         AudioClip randomAudioClip = Footsteps[Random.Range(0, Footsteps.Count - 1)];
         audioSource.PlayOneShot(randomAudioClip);
+    }
+    public void KillAlien(){
+        foreach(var script in scripts)
+            Destroy(script);
     }
 }
