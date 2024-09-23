01716f71b0ae9b1acdaf615077ff9973b005b3a0
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2024-09-23 13:39:21 -0300

    fix: aliens revive sometimes

--- Assets/Prefabs/Aliens/Scripts/AlienEvents.cs
+++ Assets/Prefabs/Aliens/Scripts/AlienEvents.cs
@@ -6,13 +6,18 @@ public class AlienEvents : MonoBehaviour
     [SerializeField] List<AudioClip> Footsteps;
     [SerializeField] AudioSource audioSource;
     [SerializeField] List<Component> scripts;
+    [SerializeField] List<GameObject> gameObjects;
     public void PlayFootstepSound()
     {
         AudioClip randomAudioClip = Footsteps[Random.Range(0, Footsteps.Count - 1)];
         audioSource.PlayOneShot(randomAudioClip);
     }
-    public void KillAlien(){
-        foreach(var script in scripts)
+    public void KillAlien()
+    {
+        foreach (var Gameobject in gameObjects)
+            Destroy(Gameobject);
+        foreach (var script in scripts)
             Destroy(script);
+
     }
 }
